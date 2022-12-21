dc97c1f8ed906ee382fed5c2d575ace675882c71
ba144220 <hs41511@gmail.com>
2022-12-21 21:44:56 +0800

    http test

---
+++ RaspberryPi/http_test.py
@@ -0,0 +1,14 @@
+import requests
+baseUrl = "https://iot-term-project-server.onrender.com/messages"
+
+r = requests.post(baseUrl, data={
+    "messageType":0,
+    "water":56,
+    "electricity": -1,
+    "senderId":"d",
+    "battery":50,
+    "path":["d", "b", "c"]
+})
+
+print(r.status_code)
+print(r.text)
